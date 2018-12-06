93dc8e283fbf18fc59c38a8d1a10a9bc2f82a6e7
Abhishek Kumar Seth <abhishekseth054@gmail.com>
2018-12-07 00:04:41 +0530

    Searching and sorting Data Structure

---
+++ Script/GetFilesRecursive.py
@@ -0,0 +1,10 @@
+def print_directory_contents(sPath):
+    import os
+    for sChild in os.listdir(sPath):
+        sChildPath = os.path.join(sPath,sChild)
+        if os.path.isdir(sChildPath):
+            print_directory_contents(sChildPath)
+        else:
+            print(sChildPath)
+
+print_directory_contents("D:\Python");
---
+++ Script/OOPS.py
@@ -0,0 +1,60 @@
+class A(object):
+    def go(self):
+        print("go A go!")
+    def stop(self):
+        print("stop A stop!")
+    def pause(self):
+        raise Exception("Not Implemented")
+
+class B(A):
+    def go(self):
+        super(B, self).go()
+        print("go B go!")
+
+class C(A):
+    def go(self):
+        super(C, self).go()
+        print("go C go!")
+    def stop(self):
+        super(C, self).stop()
+        print("stop C stop!")
+
+class D(B,C):
+    def go(self):
+        super(D, self).go()
+        print("go D go!")
+    def stop(self):
+        super(D, self).stop()
+        print("stop D stop!")
+    def pause(self):
+        print("wait D wait!")
+
+class E(B,C): pass
+
+# a = A()
+# a.go()
+# a.stop()
+# # a.pause()
+
+# b = B()
+# b.go()
+# b.stop()
+# # b.pause()
+#
+# c = C()
+# c.go()
+# c.stop()
+# # c.pause()
+
+# d = D()
+# d.go()
+# d.stop()
+# d.pause()
+#
+e = E()
+e.go()
+e.stop()
+e.pause()
+
+
+
